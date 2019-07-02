21dc3f80a60f165e0d3759db6eb4d5deef1c3ac9
Park Jin Han <wakeup5@outlook.kr>
2019-07-02 13:02:31 +0900

    Update Poolable.cs

--- Pool/Poolable.cs
+++ Pool/Poolable.cs
@@ -7,21 +7,22 @@ namespace MountainOutside
 	{
 
 	}
-    public class Poolable : Poolable<Poolable>
-    {
-    }
-    public class Poolable<T> : MonoBehaviour, IPoolable where T : Poolable<T>
-    {
+	public class Poolable : Poolable<Poolable>
+	{
+	
+	}
+	public class Poolable<T> : MonoBehaviour, IPoolable where T : Poolable<T>
+	{
 		private PoolOfPoolable<T> pool;
-        public T Original { get { return Pool?.Original; } }
-        public IPool<T> Pool 
+		public T Original { get { return Pool?.Original; } }
+		public IPool<T> Pool 
 		{ 
 			get { return pool; } 
 			internal set { pool = value as PoolOfPoolable<T>; }
 		}
-        protected virtual void OnDisable()
-        {
-            pool.Withdraw((T)this);
-        }
-    }
+		protected virtual void OnDisable()
+		{
+			pool.Withdraw((T)this);
+		}
+	}
 }
